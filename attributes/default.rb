ffa7672879e4d489477d3da221c777fb35a91aee
Piyush Verma <github@piyushverma.net>
2015-07-06 17:18:30 +0530

    Add sites to be monitored

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,15 @@
-default[:gilmour_health][:repo_prefix] = "/opt/backend_manager"
-default[:gilmour_health][:repo_path] = "#{node[:gilmour_health][:repo_prefix]}/health-bulletin"
-default[:gilmour_health][:repo_url] = "http://github.com/gilmour-libs/health-bulletin.git"
-default[:gilmour_health][:repo_branch] = "master"
-default[:gilmour_health][:repo_revision] = "HEAD"
-default[:gilmour_health][:redis_host] = "127.0.0.1"
+default[:gilmour_health][:essential_topics] = []
+
+default[:gilmour_health][:repo_prefix] = '/opt/backend_manager'
+default[:gilmour_health][:repo_path] = \
+  "#{node[:gilmour_health][:repo_prefix]}/health-bulletin"
+
+default[:gilmour_health][:repo_url] = \
+  'http://github.com/gilmour-libs/health-bulletin.git'
+
+default[:gilmour_health][:repo_branch] = 'master'
+default[:gilmour_health][:repo_revision] = 'HEAD'
+default[:gilmour_health][:redis_host] = '127.0.0.1'
 default[:gilmour_health][:redis_port] = 6379
 
-default[:vars][:user] = "ubuntu"
+default[:vars][:user] = 'ubuntu'
--- recipes/source_code.rb
+++ recipes/source_code.rb
@@ -7,15 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 
-pager_bag = data_bag_item('gilmour_health', "pagerduty")
-pagerduty_config = pager_bag[node.chef_environment] || pager_bag["_default"]
+pager_bag = data_bag_item('gilmour_health', 'pagerduty')
+pagerduty_config = pager_bag[node.chef_environment] || pager_bag['_default']
 
 user = node[:vars][:user]
 
 ssh_known_hosts_entry 'bitbucket.org'
 ssh_known_hosts_entry 'github.com'
 
-package "git"
+package 'git'
 
 directory node[:gilmour_health][:repo_path] do
   recursive true
@@ -32,46 +32,50 @@ deploy_revision node[:gilmour_health][:repo_path] do
   shallow_clone true
   keep_releases 10
   action :deploy # or :rollback
-  restart_command "touch tmp/restart.txt"
+  restart_command 'touch tmp/restart.txt'
   create_dirs_before_symlink  %w{tmp public config deploy log}
   symlink_before_migrate({})
-  #scm_provider Chef::Provider::Git
+  # scm_provider Chef::Provider::Git
   # install dependencies
   notifies :restart, 'service[gilmour_health]'
 end
 
-template File.join(node[:gilmour_health][:repo_path], "current", "config", "config.yaml") do
+params = { essential_topics: node[:gilmour_health][:essential_topics],
+           redis_host: node[:gilmour_health][:redis_host],
+           redis_port: node[:gilmour_health][:redis_port],
+           error_reporting_token: pagerduty_config['error_reporting_token'],
+           health_reporting_token: pagerduty_config['health_reporting_token'] }
+
+template File.join(node[:gilmour_health][:repo_path], 'current', 'config', 'config.yaml') do
   action :create
   backup 5
   owner user
-  source "config.yaml.erb"
-  variables :redis_host => node[:gilmour_health][:redis_host],
-    :redis_port => node[:gilmour_health][:redis_port],
-    :error_reporting_token => pagerduty_config["error_reporting_token"],
-    :health_reporting_token => pagerduty_config["health_reporting_token"]
+  source 'config.yaml.erb'
+  variables params
 end
 
+env = { 'HOME' => "/home/#{user}", 'USER' => user }
 execute 'gilmour_health_bundle_update' do
   user user
-  command "bash -c -l bundle update"
+  command 'bash -c -l bundle update'
 
   cwd "#{node[:gilmour_health][:repo_path]}/current"
   action :run
-  environment ({'HOME' => "/home/#{user}", 'USER' => user})
+  environment env
 end
 
 execute 'gilmour_health_bundle_install' do
   user user
-  command ["bash -c -l bundle install",
-           "--gemfile #{node[:gilmour_health][:repo_path]}/current/Gemfile",
-           "--path #{node[:gilmour_health][:repo_path]}/shared/bundle"
-  ].join(' ')
+  command ['bash -c -l bundle install',
+           '--gemfile #{node[:gilmour_health][:repo_path]}/current/Gemfile',
+           '--path #{node[:gilmour_health][:repo_path]}/shared/bundle'
+          ].join(' ')
 
   cwd "#{node[:gilmour_health][:repo_path]}/current"
   action :run
-  environment ({'HOME' => "/home/#{user}", 'USER' => user})
+  environment env
 end
 
-gem_package "foreman" do
+gem_package 'foreman' do
   action :install
 end
